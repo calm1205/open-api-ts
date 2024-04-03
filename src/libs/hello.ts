a30712eff1c6085cdf0ca05bd1f8d695e54b899c
calm1205 <naginani072@gmail.com>
2024-04-03 17:43:07 +0900

    clm swagger

--- src/libs/hello.ts
+++ src/libs/hello.ts
@@ -1,6 +1,6 @@
 import { client } from "./api";
 
 export const hello = async () => {
-  const { data } = await client.GET("/v2/hello");
-  console.log(data);
+  const response = await client.GET("/v2/hello");
+  console.log(response);
 };
