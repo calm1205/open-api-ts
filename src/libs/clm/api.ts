90d564b1fbc76a4e2fa8cfaabcd86d2c24c93766
calm1205 <naginani072@gmail.com>
2024-04-04 11:25:19 +0900

    rm clm api

--- src/libs/clm/api.ts
+++ src/libs/clm/api.ts
@@ -1,5 +1,5 @@
 import createClient from "openapi-fetch";
-import type { paths } from "~/schema/clm_schema";
+import type { paths } from "~/schema/org_schema";
 
 const baseUrl = "http://local1.clm.localhost:5173/api/";
 export const client = createClient<paths>({ baseUrl });
