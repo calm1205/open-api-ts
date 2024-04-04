74e0f7c0430e70bcca82d14dec270a89cfaf2d12
calm1205 <naginani072@gmail.com>
2024-04-04 11:39:10 +0900

    install swagger2openapi

--- src/libs/org/api.ts
+++ src/libs/org/api.ts
@@ -1,5 +1,5 @@
 import createClient from "openapi-fetch";
-import type { paths } from "~/schema/org_schema";
+import type { paths } from "~/schema/schema";
 
 const baseUrl = "http://localhost:9999/api/";
 export const client = createClient<paths>({ baseUrl });
--- src/libs/org/user.ts
+++ src/libs/org/user.ts
@@ -1,1 +1,22 @@
+import { client } from "~/libs/org/api";
 
+export const userApi = {
+  /** user全ての取得 */
+  getAll: async () => {
+    const response = await client.GET("/v2/user");
+    const users = response.data;
+    console.log(users);
+  },
+  getOne: async (userId: number) => {
+    const response = await client.GET(`/v2/user/{id}`, {
+      params: {
+        path: {
+          id: userId,
+        },
+      },
+    });
+
+    const user = response.data;
+    console.log(user);
+  },
+};
