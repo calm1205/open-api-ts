644ec99a19d9b0a26939740ab669ebb14339374a
calm1205 <naginani072@gmail.com>
2024-04-04 09:32:27 +0900

    add user api

---
+++ src/libs/org/api.ts
@@ -0,0 +1,5 @@
+import createClient from "openapi-fetch";
+import type { paths } from "~/schema/org_schema";
+
+const baseUrl = "http://localhost:9999/api/";
+export const client = createClient<paths>({ baseUrl });
--- src/schema/org_schema.d.ts
+++ src/schema/org_schema.d.ts
@@ -4,13 +4,79 @@
  */
 
 export interface paths {
-  "/v2/sample": {
-    /** sample api */
+  "/v2/user": {
+    /** user entityを全て取得 */
     get: {
       responses: {
-        /** get sample */
+        /** get user */
         200: {
-          schema: definitions["sampleEntity"];
+          schema: definitions["userEntities"];
+        };
+      };
+    };
+    /** userの追加 */
+    post: {
+      parameters: {
+        body: {
+          /** userのリクエスト */
+          user: definitions["userEntity"];
+        };
+      };
+      responses: {
+        /** post user */
+        200: {
+          schema: definitions["userEntity"];
+        };
+      };
+    };
+  };
+  "/v2/user/{id}": {
+    /** user(単体)の取得 */
+    get: {
+      parameters: {
+        path: {
+          /** user id */
+          id: number;
+        };
+      };
+      responses: {
+        /** get user */
+        200: {
+          schema: definitions["userEntity"];
+        };
+      };
+    };
+    /** userの更新 */
+    put: {
+      parameters: {
+        path: {
+          /** user id */
+          id: number;
+        };
+        body: {
+          /** userのリクエスト */
+          user: definitions["userEntity"];
+        };
+      };
+      responses: {
+        /** put user */
+        200: {
+          schema: definitions["userEntity"];
+        };
+      };
+    };
+    /** userの削除 */
+    delete: {
+      parameters: {
+        path: {
+          /** user id */
+          id: number;
+        };
+      };
+      responses: {
+        /** delete user */
+        200: {
+          schema: definitions["userEntity"];
         };
       };
     };
@@ -18,13 +84,23 @@ export interface paths {
 }
 
 export interface definitions {
-  sampleEntity: {
+  /** @description user entity */
+  userEntity: {
     /**
      * Format: int32
-     * @description sample id
+     * @description user id
      */
     id?: number;
+    /** @description user name */
+    name?: string;
+    /**
+     * Format: int32
+     * @description user age
+     */
+    age?: number;
   };
+  /** @description user entityの配列 */
+  userEntities: definitions["userEntity"][];
 }
 
 export interface operations {}
