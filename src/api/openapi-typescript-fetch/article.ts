ee3f4f8c4a2f41468dd8f6eb0b36e3fe9d216715
calm1205 <naginani072@gmail.com>
2024-04-15 18:46:38 +0900

    openapi typescript fetch

--- src/api/openapi-typescript-fetch/article.ts
+++ src/api/openapi-typescript-fetch/article.ts
@@ -4,10 +4,24 @@ import { paths } from "~/schema/openapi-typescript/schema";
 const fetcher = Fetcher.for<paths>();
 
 fetcher.configure({
-  baseUrl: "http://localhost:3000/api/",
+  baseUrl: "http://localhost:3000/api",
 });
 
 export const articleApi = {
-  getAll: fetcher.path("/v1/articles").method("get").create(),
-  getOne: fetcher.path("/v1/article/{id}").method("get").create(),
+  getAll: async () => {
+    const fetch = fetcher.path("/v1/articles").method("get").create();
+    const { data } = await fetch({ order: "asc", order_by: "id" });
+    console.log(data);
+    return data;
+  },
+  getOne: async (articleId: number) => {
+    const fetch = fetcher
+      .path("/v1/article/{article_id}")
+      .method("get")
+      .create();
+
+    const { data } = await fetch({ article_id: articleId });
+    console.log(data);
+    return data;
+  },
 };
