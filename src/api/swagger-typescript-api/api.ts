14b1bce81b2b734679545b62944feb14ddfbf29f
calm1205 <naginani072@gmail.com>
2024-04-16 10:09:01 +0900

    set middleware

--- src/api/swagger-typescript-api/api.ts
+++ src/api/swagger-typescript-api/api.ts
@@ -3,3 +3,16 @@ import { Api } from "~/schema/swagger-typescript-api/Api";
 export const api = new Api({
   baseURL: "http://localhost:3000/api/",
 });
+
+export const axiosInstance = api.instance;
+axiosInstance.interceptors.request.use(
+  (response) => {
+    console.log(response);
+    return {
+      ...response,
+    };
+  },
+  (error) => {
+    console.error(error);
+  }
+);
